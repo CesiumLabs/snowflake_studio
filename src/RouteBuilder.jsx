aa6efc4f41483b9a07f8b1e745d5683a8fff4c40
Snowflake <devsnowflake107@gmail.com>
2020-09-16 14:03:54 +0545

    Update RouteBuilder.jsx

--- src/RouteBuilder.jsx
+++ src/RouteBuilder.jsx
@@ -6,15 +6,12 @@ import ErrorPage from './ErrorPage';
 import RedirectURI from './RedirectURI';
 import Support from './Support';
 
-const AUTH_URI = `https://discordapp.com/api/oauth2/authorize?client_id=648875676186968065&scope=identify&redirect_uri=${encodeURIComponent("http://localhost:3500/api/auth/callback")}`;
-
 export default function RouteBuilder() {
     return (
         <>
             <Navbar />
             <Switch>
                 <Route path="/" exact component={Home} />
-                <Route path="/login" exact component={() => <RedirectURI url={AUTH_URI}/>} />
                 <Route path="/support" exact component={Support} />
                 <Route path="/" component={ErrorPage} />
             </Switch>
